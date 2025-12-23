b4cc595a5665982eb36ce5553c6c3332adb05949
Dickson Souza <36424026+disouzam@users.noreply.github.com>
2025-12-23 14:09:13 -0300

    Moved first cells to the botton of the list of cells to test marimo's to Jupyter export

--- section01-Numpy-Primer/section-01-assignment-5.py
+++ section01-Numpy-Primer/section-01-assignment-5.py
@@ -12,46 +12,6 @@ def _():
 
 
 @app.cell
-def _():
-    import numpy as np
-
-    return (np,)
-
-
-@app.cell(hide_code=True)
-def _(mo):
-    mo.md(r"""
-    # Assignment 5: Filtering Arrays
-
-    Filter the product array to only include those with prices greater than 25.
-
-    Modify your logic to include cola, despite it not having a price greater than 25.
-    Store the elements returned in an array called `fancy_feast_special`.
-
-    Next, create a shipping cost array where the cost is 0 if price is greater than 20, and 5 if not.
-    """)
-    return
-
-
-@app.cell
-def _(np):
-    prices = np.array([5.99, 6.99, 22.49, 99.99, 4.99, 49.99])
-    products = np.array(
-        ["salad", "bread", "mustard", "rare tomato", "cola", "gourmet ice cream"]
-    )
-
-    print(products)
-    print(prices)
-    zipped_list = list(zip(products, prices))
-
-    print()
-
-    for item in zipped_list:
-        print(item)
-    return prices, products
-
-
-@app.cell
 def _(prices, products):
     # Products with prices greater than 25
     over25_prices = products[prices > 25]
@@ -108,6 +68,46 @@ def _(np, prices):
     shipping_cost_array_2[shipping_cost_mask] = 5
     print(shipping_cost_array_2)
     return
+
+
+@app.cell
+def _():
+    import numpy as np
+
+    return (np,)
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Assignment 5: Filtering Arrays
+
+    Filter the product array to only include those with prices greater than 25.
+
+    Modify your logic to include cola, despite it not having a price greater than 25.
+    Store the elements returned in an array called `fancy_feast_special`.
+
+    Next, create a shipping cost array where the cost is 0 if price is greater than 20, and 5 if not.
+    """)
+    return
+
+
+@app.cell
+def _(np):
+    prices = np.array([5.99, 6.99, 22.49, 99.99, 4.99, 49.99])
+    products = np.array(
+        ["salad", "bread", "mustard", "rare tomato", "cola", "gourmet ice cream"]
+    )
+
+    print(products)
+    print(prices)
+    zipped_list = list(zip(products, prices))
+
+    print()
+
+    for item in zipped_list:
+        print(item)
+    return prices, products
 
 
 if __name__ == "__main__":
